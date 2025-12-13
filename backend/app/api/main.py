7a3497d866ee14e308159d8a2f2e635687e9585d
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-12-13 05:45:17 -0800

    feat(backend): add routes for get, create, update & search routes in /sweets

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.routes import auth
+from app.api.routes import sweets
 
 
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth.router)
+api_router.include_router(sweets.router)
