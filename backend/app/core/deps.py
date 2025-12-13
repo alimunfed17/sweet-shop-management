8ed5704f5324cc62fa0e1f1355e08e3a18b98849
Munfed Ali <alimunfed@Munfeds-MacBook-Air.local>
2025-12-13 06:06:08 -0800

    feat: add get_current_admin_user dependency

--- backend/app/core/deps.py
+++ backend/app/core/deps.py
@@ -39,3 +39,14 @@ def get_current_user(
         )
     
     return user
+
+
+def get_current_admin_user(
+    current_user: User = Depends(get_current_user)
+) -> User:
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Not enough permissions"
+        )
+    return current_user
